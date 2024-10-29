bd1e5cbab0c75386368fa03f6d8b1b84a96bc240
Arnold Parreco <arnold.parreco@gmail.com>
2024-10-29 01:35:37 +0000

    testing typescript

---
+++ frontend/src/components/IngredientList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IngredientList: React.FC = () => {
+    return (
+        <div>
+            <h2>Your Ingredients</h2>
+            {/* Ingredients will go here */}
+        </div>
+    );
+};
+
+export default IngredientList;
