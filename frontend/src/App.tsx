d8c99d37b68805fe02bc2d6b95b53a7fba8ba200
Arnold Parreco <arnold.parreco@gmail.com>
2024-11-01 19:58:08 +0000

    Added database, api, landing page, sign up page, and login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage'; // Import the new LandingPage component
+import Signup from './components/Signup'; //Imports signup
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; //imports routing
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Routes>
       {/* Render the LandingPage component */}
-      <LandingPage />
+      <Route path = "/" element = {<LandingPage />}/>
+      <Route path = "/signup" element = {<Signup />} />
+      <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './LandingPage.css';
+import {useNavigate } from 'react-router-dom'; 
 
 const LandingPage: React.FC = () => {
+  const navigate = useNavigate(); 
   return (
     <div className="landing-page">
       <h1>Welcome to the App!</h1>
       <p>Your go-to app for recipe inspiration based on ingredients you have.</p>
       <div className="auth-buttons">
         <button onClick={() => alert('Navigate to Login')}>Login</button>
-        <button onClick={() => alert('Navigate to Sign Up')}>Sign Up</button>
+        <button onClick={() => navigate('/signup')}>Sign Up</button>
       </div>
     </div>
   );
