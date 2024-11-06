982ff80b01d9e262501e6a009f381f411a1bdb13
Arnold Parreco <arnold.parreco@gmail.com>
2024-11-06 14:51:40 +0000

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-require('dotenv').config();
-const url = process.env.MONGODB_URL;
 
 const app = express();
 app.use(cors());
@@ -20,45 +12,46 @@ const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
-
-app.post('/api/login', async (req, res, next) => {
-  const { Login, Password } = req.body;
-  const db = client.db();
-  const results = await db.collection('Users').find({ login: Login, password: Password }).toArray();
-
-  let fn = '';
-  let ln = '';
-  if (results.length > 0) {
-    fn = results[0].first_name;
-    ln = results[0].last_name;
-  }
-
-  res.status(200).json({ first_name: fn, last_name: ln, error: '' });
-});
-
-app.listen(5000); // start Node + Express server on port 5000
+app.post('/api/login', async (req, res, next) => 
+  {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
     
+   var error = '';
+  
+    const { login, password } = req.body;
+  
+    const db = client.db('POOSD-Large-Project');
+    const results = await db.collection('Users').find({login:login,password:password}).toArray();
+  
+    var id = -1;
+    var fn = '';
+    var ln = '';
+  
+    if( results.length > 0 )
+    {
+      id = results[0]._id;
+      fn = results[0].first_name;
+      ln = results[0].last_name;
+    }
+  
+    var ret = { id:id, first_name:fn, last_name:ln, error:''};
+    res.status(200).json(ret);
+});
+
+app.use((req, res, next) => 
+  {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+  });
+  
+app.listen(5000); // start Node + Express server on port 5000
+  
